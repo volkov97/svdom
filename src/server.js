50d60c388ea5a4b2860739b6de90e819fde4028c
German Volkov <volkov.german.1997@gmail.com>
2021-05-18 03:36:58 +0300

    pong with json

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/ping', (req, res) => res.end('pong'));
+app.get('/ping', (req, res) => res.json({ ping: 'pong' }));
 
 const PORT = 3000;
 
